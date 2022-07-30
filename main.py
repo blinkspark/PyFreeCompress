6a6ffc1b52bdd8f2ce5c8083e951a68b296dbb8f
blinkspark <3829294+blinkspark@users.noreply.github.com>
2022-07-30 16:20:10 +0800

    double click and context menu

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QWidget, QGridLayout, QMessageBox, QListWidget, QListWidgetItem, QTreeWidgetItem, QStyle
-from PySide6.QtCore import Qt, QCoreApplication, QSize
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QWidget, QGridLayout, QMessageBox, QListWidget, QListWidgetItem, QTreeWidgetItem, QStyle, QMenu
+from PySide6.QtCore import Qt, QCoreApplication, QSize, Slot, Signal
 from PySide6.QtGui import QPixmap, QIcon, QCloseEvent
 import sys, os
 from PIL import ImageQt, Image
 
 
 class MyApp(QMainWindow):
+  updateFileListSignal = Signal()
 
   def __init__(self):
     super().__init__()
@@ -29,9 +30,22 @@ class MyApp(QMainWindow):
     self.fileMenu.addAction(self.tr('Exit'), self.exit)
 
     self.fileListView = QListWidget()
+    self.fileListView.setContextMenuPolicy(Qt.CustomContextMenu)
+    self.fileListView.customContextMenuRequested.connect(
+        self.onFileListViewContextMenu)
+    self.fileListView.itemDoubleClicked.connect(self.onItemDoubleClicked)
     self.fileListView.setSelectionMode(QListWidget.ExtendedSelection)
     self.mainLayout.addWidget(self.fileListView, 0, 0)
-    self.updateFileList()
+    self.updateFileListSignal.connect(self.updateFileList)
+    self.updateFileListSignal.emit()
+
+    menu = QMenu()
+    menu.addAction(self.tr('Extract'))
+    menu.addAction(self.tr('Create zip file'))
+    menu.addAction(self.tr('Create 7z file'))
+    menu.addAction(self.tr('Create tar.gz file'))
+    menu.addAction(self.tr('Create tar.xz file'))
+    self.fileListViewContextMenu = menu
 
     btn = QPushButton(self.tr('Open'), self)
     btn.clicked.connect(self.debugClicked)
@@ -42,21 +56,51 @@ class MyApp(QMainWindow):
     si = self.fileListView.selectedItems()
     print(si)
 
+  def onFileListViewContextMenu(self, pos):
+    print(pos)
+    items = self.fileListView.selectedItems()
+    for item in items:
+      print(item.data(Qt.UserRole))
+    menu = self.fileListViewContextMenu
+    menu.exec(self.fileListView.mapToGlobal(pos))
+
   def updateFileList(self):
     self.fileListView.clear()
-    for file in os.listdir(self.currentDir):
-      icon = None
-      if os.path.isdir(os.path.join(self.currentDir, file)):
+    icon = None
+    isDir = False
+    fpath = ""
+    dirList = ['..']
+    dirList.extend(os.listdir(self.currentDir))
+    for file in dirList:
+      if file == '..':
+        fpath = os.path.abspath(os.path.join(self.currentDir, os.pardir))
+        isDir = True
+      else:
+        fpath = os.path.join(self.currentDir, file)
+        isDir = os.path.isdir(fpath)
+
+      if isDir:
         icon = QStyle.SP_DirIcon
       else:
         icon = QStyle.SP_FileIcon
       icon = self.style().standardIcon(icon)
-      print(icon)
       item = QListWidgetItem(icon, file, self.fileListView)
+      item.setData(Qt.UserRole, {"isDir": isDir, "path": fpath})
       item.setIcon(icon)
       # item.setSizeHint(QSize(100, 100))
       # self.fileListView.addItem(file)
       self.filelistViewItems.append(item)
+
+  @Slot(QListWidgetItem)
+  def onItemDoubleClicked(self, item: QListWidgetItem):
+    data = item.data(Qt.UserRole)
+    print('item:', data)
+    if data is None:
+      return
+    if data["isDir"]:
+      self.currentDir = data["path"]
+      # self.updateFileList()
+      self.updateFileListSignal.emit()
 
   def closeEvent(self, event: QCloseEvent) -> None:
     mb = QMessageBox()

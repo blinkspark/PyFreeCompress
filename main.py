157f8226f85ced98aacf40d20c87d3214b042151
blinkspark <3829294+blinkspark@users.noreply.github.com>
2022-08-10 13:35:46 +0800

    zip with progress dialog

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QWidget, QGridLayout, QMessageBox, QListWidget, QListWidgetItem, QTreeWidgetItem, QStyle, QMenu, QFileDialog
+from tracemalloc import take_snapshot
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QWidget, QGridLayout, QMessageBox, QListWidget, QListWidgetItem, QTreeWidgetItem, QStyle, QMenu, QFileDialog, QProgressDialog
 from PySide6.QtCore import Qt, QCoreApplication, QSize, Slot, Signal
 from PySide6.QtGui import QPixmap, QIcon, QCloseEvent
 import sys, os
+from os import path
 from PIL import ImageQt, Image
 import zipfile as zf
+
 
 class MyApp(QMainWindow):
   updateFileListSignal = Signal()
@@ -46,45 +49,86 @@ class MyApp(QMainWindow):
     menu.addAction(self.tr('Create tar.xz file'))
     self.fileListViewContextMenu = menu
 
-    btn = QPushButton(self.tr('Open'), self)
+    btn = QPushButton(self.tr('Debug'), self)
     btn.clicked.connect(self.debugClicked)
     self.mainLayout.addWidget(btn, 1, 0)
+
+  def walkDir(self, dirPath: str) -> list[tuple[str, str]]:
+    parDir: str = path.abspath(path.join(dirPath, os.pardir))
+    dirList = []
+    for root, _, files in os.walk(dirPath):
+      for file in files:
+        fpath = path.join(root, file)
+        dirList.append((fpath, path.relpath(fpath, parDir)))
+    return dirList
+
+  def getTaskData(self) -> list[tuple[str, str]]:
+    taskData: list[tuple[str, str]] = []
+    for item in self.fileListView.selectedItems():
+      data = item.data(Qt.UserRole)
+      fname = data['path']
+      if data['isDir']:
+        # self.writeZipDir(fname, path.basename(fname), zipFile)
+        tds = self.walkDir(fname)
+        taskData.extend(tds)
+      else:
+        # zipFile.write(fname, path.basename(fname))
+        td = (fname, path.basename(fname))
+        taskData.append(td)
+    return taskData
 
   def onCreateZip(self):
     fpath = QFileDialog.getSaveFileName(self, self.tr('Select zip file path'),
                                         self.currentDir,
                                         self.tr('Zip file (*.zip)'))
-    self.makeZip(fpath)
+    self.makeZip(fpath[0])
 
-  def makeZip(self, fpath, comporessionType=zf.ZIP_DEFLATED, compressLevel=9):
-    zipFile = zf.ZipFile(fpath[0], 'w', comporessionType, compresslevel=compressLevel)
-    for item in self.fileListView.selectedItems():
-      data = item.data(Qt.UserRole)
-      fname = data['path']
-      if data['isDir']:
-        self.writeZipDir(fname, os.path.basename(fname), zipFile)
-      else:
-        zipFile.write(fname, os.path.join(os.path.basename(fname)))
+  def makeZip(self,
+              fpath: str,
+              comporessionType=zf.ZIP_DEFLATED,
+              compressLevel=6):
+    zipFile = zf.ZipFile(fpath,
+                         'w',
+                         comporessionType,
+                         compresslevel=compressLevel)
+    taskData = self.getTaskData()
+    taskCount = len(taskData)
+    progressDialog = QProgressDialog(self.tr('Compressing...'),
+                                     self.tr('Cancel'), 0, taskCount)
+    progressDialog.setWindowModality(Qt.ApplicationModal)
+    progressDialog.setMinimumDuration(1)
+    progressDialog.can
+    # progressDialog.setValue(0)
+    progressDialog.show()
+    for (i, td) in enumerate(taskData):
+      print(i + 1, taskCount, td)
+      progressDialog.setValue(i + 1)
+      progressDialog.setLabelText(path.basename(td[1]))
+      zipFile.write(td[0], td[1])
+      # time.sleep(0.1)
     zipFile.close()
-    print('zip file created')
 
   def writeZipDir(self, dirPath, dirName, zipFile):
     for fname in os.listdir(dirPath):
-      fpath = os.path.join(dirPath, fname)
-      if os.path.isdir(fpath):
-        self.writeZipDir(fpath, os.path.join(dirName, fname), zipFile)
+      fpath = path.join(dirPath, fname)
+      if path.isdir(fpath):
+        self.writeZipDir(fpath, path.join(dirName, fname), zipFile)
       else:
-        zipFile.write(fpath, os.path.join(dirName, fname))
+        zipFile.write(fpath, path.join(dirName, fname))
 
     # self.label.setContextMenuPolicy(Qt.CustomContextMenu)
   def debugClicked(self):
-    si = self.fileListView.selectedItems()
-    print(si)
+    for item in self.fileListView.selectedItems():
+      data = item.data(Qt.UserRole)
+      fpath = data['path']
+      if data['isDir']:
+        pardir = path.abspath(path.join(fpath, os.pardir))
+        print(f'pardir:{pardir}')
+        for f in self.walkDir(fpath):
+          print(f)
 
   def onFileListViewContextMenu(self, pos):
     items = self.fileListView.selectedItems()
-    for item in items:
-      print(item.data(Qt.UserRole))
     menu = self.fileListViewContextMenu
     menu.exec(self.fileListView.mapToGlobal(pos))
 
@@ -97,11 +141,11 @@ class MyApp(QMainWindow):
     dirList.extend(os.listdir(self.currentDir))
     for file in dirList:
       if file == '..':
-        fpath = os.path.abspath(os.path.join(self.currentDir, os.pardir))
+        fpath = path.abspath(path.join(self.currentDir, os.pardir))
         isDir = True
       else:
-        fpath = os.path.join(self.currentDir, file)
-        isDir = os.path.isdir(fpath)
+        fpath = path.join(self.currentDir, file)
+        isDir = path.isdir(fpath)
 
       if isDir:
         icon = QStyle.SP_DirIcon
@@ -125,17 +169,18 @@ class MyApp(QMainWindow):
       self.updateFileListSignal.emit()
 
   def closeEvent(self, event: QCloseEvent) -> None:
-    mb = QMessageBox()
-    mb.setText(self.tr('Are you sure you want to quit?'))
-    mb.setIcon(QMessageBox.Question)
-    mb.setStandardButtons(QMessageBox.Ok.Yes | QMessageBox.No)
-    mb.setDefaultButton(QMessageBox.No)
-    ret = mb.exec()
-    if ret == QMessageBox.Yes:
-      # event.accept()
-      super().closeEvent(event)
-    else:
-      event.ignore()
+    pass
+    # mb = QMessageBox()
+    # mb.setText(self.tr('Are you sure you want to quit?'))
+    # mb.setIcon(QMessageBox.Question)
+    # mb.setStandardButtons(QMessageBox.Ok.Yes | QMessageBox.No)
+    # mb.setDefaultButton(QMessageBox.No)
+    # ret = mb.exec()
+    # if ret == QMessageBox.Yes:
+    #   # event.accept()
+    #   super().closeEvent(event)
+    # else:
+    #   event.ignore()
 
   def exit(self):
     QApplication.instance().quit()
